5c03b635c240324e52bce9820afbc75bfd65dc9f
Luckyy2 <bimolp@student.telkomuniversity.ac.id>
2023-05-28 17:05:00 +0700

    Add login page

---
+++ src/pages/LandingPage.svelte
@@ -0,0 +1,37 @@
+<script>
+    import './lib/app.css'
+</script>
+
+<style>
+    img{
+        width: 200px;
+        height:  200px;
+
+    }
+</style>
+<div class="w-full items-center flex justify-center mt-40">
+    <img src="./assets/imgs/logocheckbund.png" alt="">
+</div>
+
+<div class="w-full items-center flex justify-center mt-10">
+    <div class="flex mt-5">
+        <div class="m-3">
+          <button
+            class="w-72 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+            <span class="mx-auto">Login</span>
+          </button>
+        </div>
+      </div>
+</div>
+
+<div class="w-full items-center flex justify-center">
+    <div class="flex mt-5">
+        <div class="m-3">
+          <button
+            class="w-72 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+            <span class="mx-auto">Register</span>
+          </button>
+        </div>
+      </div>
+</div>
+
--- src/pages/login.svelte
+++ src/pages/login.svelte
@@ -2,36 +2,30 @@
     import './lib/app.css'
 </script>
 
-<style>
-    img{
-        width: 200px;
-        height:  200px;
-
-    }
-</style>
-<div class="w-full items-center flex justify-center mt-40">
-    <img src="./assets/imgs/logocheckbund.png" alt="">
-</div>
-
-<div class="w-full items-center flex justify-center mt-10">
-    <div class="flex mt-5">
-        <div class="m-3">
-          <button
-            class="w-72 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-            <span class="mx-auto">Login</span>
-          </button>
+<div class="container mx-auto flex justify-center items-center h-screen">
+    <div class="w-full max-w-md">
+      <h2 class="text-3xl font-bold text-center mb-8">Login to Your Account</h2>
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Username
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username">
         </div>
-      </div>
-</div>
-
-<div class="w-full items-center flex justify-center">
-    <div class="flex mt-5">
-        <div class="m-3">
-          <button
-            class="w-72 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-            <span class="mx-auto">Register</span>
-          </button>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password">
         </div>
-      </div>
-</div>
-
+        <div class="flex items-center justify-between">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Sign In
+          </button>
+          <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+    </div>
+  </div>
