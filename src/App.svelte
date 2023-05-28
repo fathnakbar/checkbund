bf8d610910cc0090360f49f2d6157ba7624cad4b
fathnakbar <fathnakbar@gmail.com>
2023-05-28 17:03:25 +0700

    Setup storybook for visual testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,11 @@
+<script>
+    import "./lib/app.css"
+</script>
+
 <a href="google.com/s='Judi online'">Judi online</a>
+
+<style>
+    a {
+        
+    }
+</style>
---
+++ src/stories/Register.stories.js
@@ -0,0 +1,14 @@
+import Page from "../pages/Register.svelte";
+
+export default {
+  title: 'Page/Register',
+  component: Page,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/svelte/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/svelte/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {}
