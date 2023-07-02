f3c246283d0c8cf2724ee923b99ebcd825364a09
fathnakbar <fathnakbar@gmail.com>
2023-07-02 17:17:29 +0700

    Fix ListCatatan to show data

--- src/lib/components/ListCatatan.svelte
+++ src/lib/components/ListCatatan.svelte
@@ -3,16 +3,26 @@
   import LabledValue from "./LabledValue.svelte";
   import Schedule from "../assets/icons/schedule.svelte";
   import { useProps } from "../client";
+  import { each } from "svelte/internal";
 
   export let data_catatan;
-  let amount;
 
-  onMount(() => {
-    amount = useProps(data_catatan?.catatan, "tekanan_darah,berat_badan,lingkar_perut,metode_kontrasepsi,umur_kehamilan")
-  })
+  let notes = useProps(
+    data_catatan.catatan ?? {},
+    "nasihat,keluhan,hasil_pemeriksaan"
+  );
+  notes = Object.entries(notes);
+
+  let amount = useProps(
+    data_catatan.catatan ?? {},
+    "tekanan_darah,berat_badan,lingkar_perut,metode_kontrasepsi,umur_kehamilan"
+  );
 
   let show_detail;
 
+  $: info = show_detail
+    ? Object.entries(amount)
+    : Object.entries(amount).slice(0, 2);
 
   function formatDate(dateString) {
     const date = new Date(dateString);
@@ -23,13 +33,11 @@
     };
     const formatter = new Intl.DateTimeFormat("en-US", options);
 
-    console.log(data_catatan)
-
     return dateString ? formatter.format(date) : "Tidak tersedia";
   }
 
   function toggleDetail() {
-    show_detail = !show_detail
+    show_detail = !show_detail;
   }
 </script>
 
@@ -43,19 +51,42 @@
   </div>
   <div class="grid grid-cols-2">
     {#if amount}
-    {#each Object.entries(amount) as [key, value]}
-    <div class="leading-snug">
-      <span class="text-gray-500 text-sm">Tekanan darah:</span>
-      <br />
-      <span class="text-sm">120/80 mmHg</span>
-    </div>
-  {/each}
+      {#each info as [key, value]}
+        <div class="leading-snug mb-3">
+          <span class="text-gray-500 text-sm"
+            >{key[0].toUpperCase()}{key.slice(1).replace("_", " ")}</span
+          >
+          <br />
+          <span class="text-sm">{value}</span>
+        </div>
+      {/each}
     {/if}
   </div>
+  <div class="my-3"></div>
+  {#if show_detail}
+    {#each notes as [key, value]}
+      <div class="my-5">
+        <div class="mb-1 font-bold text-sm">
+          {key[0].toUpperCase()}{key.slice(1).replace("_", " ")}
+        </div>
+        <p class="text-sm">{value ?? "Tidak ada"}</p>
+      </div>
+    {/each}
+  {:else if notes.length > 0 && notes[0] != undefined && notes[0][1] != undefined}
+    <div class="text-sm mb-1 font-bold">
+      {((key) => `${key[0].toUpperCase()}${key.slice(1).replace("_", " ")}`)(
+        notes[1][0]
+      )}
+    </div>
+    <textarea rows="2" class="text-sm p-0 border-0 w-full" value={notes[1][1]} disabled/>
+  {/if}
+
   <div class="flex justify-between text-sm mt-5">
-    <div class="text-gray-500">Dicatat <b>{data_catatan?.user_data?.name}</b></div>
+    <div class="text-gray-500">
+      Dicatat <b>{data_catatan?.user_data?.name}</b>
+    </div>
     <button class="text-blue-500 font-bold" on:click={toggleDetail}
-      >lihat detail</button
+      >{!show_detail ? "lihat detail" : "sembunyikan"}</button
     >
   </div>
 </div>
